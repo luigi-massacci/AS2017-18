f1e8f1ef3e573be440a7b00896c1442053b4974f
Luigi Massacci <luigi.massacci.it@gmail.com>
2017-12-17 18:54:15 +0100

    added lib/examples dir

--- src/lib/examples/ToF_example.ino
+++ src/lib/examples/ToF_example.ino
@@ -1,25 +1,26 @@
 #include <Wire.h>
 #include <VL6180X.h>
-#define reset1 x    //pins for hardware reset of sensors, attached to transistors to let 3.3V flow to the GPIO0 on the board 
-#define reset2 x
+
+const int reset1 = 3;   //pins for hardware reset of sensors, attached to transistors to let 3.3V flow to the GPIO0 on the board
+const int reset2  = 4;
 
 VL6180X sensore1;   //creating objects for both sensors
-VL6180X sensore2;   
+VL6180X sensore2;
 
 
 void setup(){
-  
+
     Serial.begin(9600);
     Wire.begin();
     pinMode(reset1, OUTPUT);    //setting both reset pins in OUTPUT mode
-    pinMode(reset2, OUTPUT);    
+    pinMode(reset2, OUTPUT);
     digitalWrite(reset2, HIGH); //disabling boot on chip #2
     sensore1.setAddress(0x22);
     digitalWrite(reset1, HIGH);
     digitalWrite(reset2, LOW);
     sensore2.setAddress(0x25);
-    
-    sensore1.init();          
+
+    sensore1.init();
     sensore1.configureDefault();
     sensore1.writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 30);
     sensore1.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);  //initialization of sensor #1
@@ -27,8 +28,8 @@ void setup(){
     sensore1.stopContinuous();
     delay(300);
     sensore1.startInterleavedContinuous(100);
-    
-    sensore2.init();          
+
+    sensore2.init();
     sensore2.configureDefault();
     sensore2.writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 30);
     sensore2.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);  //initialization of sensor #2
@@ -49,7 +50,7 @@ void loop()
     Serial.print("Range sensor 2: ");
     Serial.print(sensore2.readRangeContinuousMillimeters());
     if (sensore2.timeoutOccurred()) {
-        Serial.println(" TIMEOUT"); 
+        Serial.println(" TIMEOUT");
     }
 
   Serial.println();
