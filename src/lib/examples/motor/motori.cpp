bb6c3626e19642402ad8d959e33c44eea3d0f2f9
Unknown <nicola.taddei01@gmail.com>
2018-01-31 16:41:48 +0100

    actual motor class template

--- src/lib/examples/motor/motori.cpp
+++ src/lib/examples/motor/motori.cpp
@@ -1,20 +1,34 @@
 
 #include "motori.h"
 
-void motori::motori(int pin1, int pin2, int pin3, int pin4){
-  this.pin_dx1=pin1;
-  this.pin_dx2=pin2;
-  this.pin_sx1=pin3;
-  this.pin_sx2=pin4;
+motori::motori( int pin1, int pin2, int pin3, int pin4 ){
+  pin_dx1 = pin1;
+  pin_dx2 = pin2;
+  pin_sx1 = pin3;
+  pin_sx2 = pin4;
 }
 
-void motori::avanti(){
-  analogWrite ( this.pin_dx1, MAX_V );
-  analogWrite ( this.pin_sx1, MAX_V);
-  analogWrite ( this.pin_dx2, MAX_V);
-  analogWrite ( this.pin_sx2, MAX_V);
+void motori::avanti( int speed){
+  move ( pin_dx1, speed );
+  move ( pin_sx1, speed );
+  move ( pin_dx2, speed );
+  move ( pin_sx2, speed );
 }
 
-void motori::curva(){
+void motori::curva( int sx_v, int dx_v ){
+  move ( pin_dx1, dx_v );
+  move ( pin_dx2, dx_v );
+  move ( pin_sx1, sx_v );
+  move ( pin_sx2, sx_v );
+}
 
+void motori::gira_per( int a )
+{
+  while ( angle() <=  a )
+  {                       // angle() restituisce l'angolo misurato dall'imu
+    move ( pin_dx1, -a/abs(a)*MIN_V );
+    move ( pin_dx2, -a/abs(a)*MIN_V );
+    move ( pin_sx1, a/abs(a)*MIN_V );
+    move ( pin_sx2, a/abs(a)*MIN_V );
+  }
 }
--- src/lib/examples/motor/motori.h
+++ src/lib/examples/motor/motori.h
@@ -3,10 +3,14 @@
 
 const int MAX_V = 255;
 
+const int MIN_V = 100;
+
 class motori{
-  int pin_dx1, pin_dx2, pin_sx1, pin_sx2;  //1 stays for "forward" and 2 stays "backward"
+  private:
+    int pin_dx1, pin_dx2, pin_sx1, pin_sx2;  //1 stays for "forward" and 2 stays "backward"
+    void move(int, int);
   public:
-    void motori(int, int, int, int);
+    motori(int, int, int, int);
     void avanti(int);
     void curva(int, int);
     void gira_per(int);
