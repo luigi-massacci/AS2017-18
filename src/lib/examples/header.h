6e3987fb70878a8df4c52c7fa6f41fd4bdf81899
Luigi Massacci <luigi.massacci.it@gmail.com>
2018-02-22 16:19:44 +0000

    header.h created online with Bitbucket

---
+++ src/lib/examples/header.h
@@ -0,0 +1,25 @@
+#ifndef MOTORE_H
+#define MOTORE_H
+
+
+
+class Motori{
+  private:
+    int pin_dx1, pin_dx2, pin_sx1, pin_sx2;  //1 stays for "forward" and 2 stays "backward"
+  public:
+    motori(int, int, int, int);
+    void move(int, int);
+    void gira_di(int);
+}
+
+class PID{
+  private:
+    int Kp, Ki, Kd;
+    double P, I, D, E;
+  public:
+    PID();
+    PID(int ,int ,int);
+    int pid(int*);
+};
+
+#endif
