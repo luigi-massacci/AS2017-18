f5d56090fc4dc78bc89069aa0a53fd85ff890f65
Luigi Massacci <luigi.massacci.it@gmail.com>
2018-02-23 16:54:16 +0000

    main.ino edited online with Bitbucket

--- src/lib/examples/main.ino
+++ src/lib/examples/main.ino
@@ -1,32 +1,30 @@
 #include <Wire.h>
 #include "motori.h"
-#define uchar unsigned char
-uchar t;
-Motori M;
-PID pidous;
+int t;
+Motori M(3, 6, 10, 11);
+PID pidous(-10, -10, -10);
 
 
 const int MAX_V = 255;
-
 const int MIN_V = 100;
-
 const int STD_V = 200;
 
-//void send_data(short a1,short b1,short c1,short d1,short e1,short f1);
-uchar data[8];
+int data[8];
+
 void setup()
 {
-  Wire.begin();        // join i2c bus (address optional for master)
-  Serial.begin(9600);  // start serial for output
+  Wire.begin();
+  Serial.begin(9600);
   t = 0;
 }
+
 void loop()
 {
-  Wire.requestFrom(9, 16);    // request 16 bytes from slave device #9
-  while (Wire.available())   // slave may send less than requested
+  Wire.requestFrom(9, 16);
+  while (Wire.available())
   {
-    if(!(t%2)) 
-      data[t/2] = Wire.read(); // receive a byte as character
+    if(!(t%2))
+      data[t/2] = Wire.read();
     if (t < 15)
       t++;
     else{
@@ -34,6 +32,6 @@ void loop()
       M.move(STD_V+pidous.pid(data), STD_V-pidous.pid(data));
     }
   }
-  
+
   delay(500);
 }
