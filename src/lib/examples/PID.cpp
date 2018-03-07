d58820dd9add04a55a292b45eea635ec19b8216f
Gabriele Rolleri <gabriele@irolleri.it>
2018-03-07 13:58:25 +0000

    PID.cpp created online with Bitbucket

---
+++ src/lib/examples/PID.cpp
@@ -0,0 +1,49 @@
+#include "PID.h"
+#include "costanti.h"
+#include <Arduino.h>
+
+PID::PID(){
+  Kp=10;
+  Ki=10;
+  Kd=10;
+  E=0;
+  I=0;
+}
+
+PID::PID(int p, int i, int d){
+  Kp=p;
+  Ki=i;
+  Kd=d;
+  E=0;
+  I=0;
+}
+
+int PID::pid_int(int* vet){
+  int M=0, S=0;
+  for(int i=0; i<8; i++)
+    M+=vet[i]*(i+1), S+=vet[i];
+  double E_prec=E;
+  E=4.5-M/S;
+  if(-0.1<E && E<0.1)
+    E_prec=0, I=0;
+  P=E*Kp;
+  I+=E*Ki;
+  D=(E-E_prec)*Kd;
+  return P+I+D;
+}
+
+int PID::pid_bool(int* vet){
+  int M=0, S=0;
+  for(int i=0; i<8; i++)
+    vet[i]>BIANCO ? vet[i]=1 : vet[i]=0;
+  for(int i=0; i<8; i++)
+    M+=vet[i]*(i+1), S+=vet[i];
+  double E_prec=E;
+  E=4.5-M/S;
+  if(-0.1<E && E<0.1)
+    E_prec=0, I=0;
+  P=E*Kp;
+  I+=E*Ki;
+  D=(E-E_prec)*Kd;
+  return P+I+D;
+}
