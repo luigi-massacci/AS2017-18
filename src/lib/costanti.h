cfad1311624e3c3afbb1467ad4f240cef726db52
nicola.taddei01@gmail.com <nicola.taddei01@gmail.com>
2018-03-14 15:23:43 +0000

    aggiunte costanti per l'aggiramento ostacoli

--- src/lib/costanti.h
+++ src/lib/costanti.h
@@ -6,6 +6,9 @@ const int MAX_V = 255;
 const int MIN_V = 210;
 const int STD_V = 210;
 const int BIANCO = 60;
+const int TEMPO_ROTAZIONE = 0;
+const int TEMPO_AGGIRAMENTO = 0;
+const int SPAZIO_OSTACOLO = 0;
 
 
 #endif
